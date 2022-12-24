81f8207b1d18ea5dce2818dbdf5d236db3325545
kurhadep06@gmail.com <kurhadep06@gmail.com>
2022-12-24 20:13:08 +0530

    Second commit :second code

---
+++ src/com/velocity/gits/LinkedListDemo.java
@@ -0,0 +1,13 @@
+package com.velocity.gits;
+
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+public static void main(String[] args) {
+	LinkedList<Integer> ll= new LinkedList<Integer>();
+	ll.add(5);
+	ll.add(6);
+	ll.add(7);
+	System.out.println(ll);
+}
+}
