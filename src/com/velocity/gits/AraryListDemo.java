4ab6140a303276ad51cd13a24b267f63ad09e262
kurhadep06@gmail.com <kurhadep06@gmail.com>
2022-12-24 20:10:59 +0530

    first commit :code

---
+++ src/com/velocity/gits/AraryListDemo.java
@@ -0,0 +1,14 @@
+package com.velocity.gits;
+
+import java.util.ArrayList;
+
+public class AraryListDemo {
+
+	public static void main(String[] args) {
+		ArrayList<String> al= new ArrayList<String>();
+		al.add("Pritam");
+		al.add("Kshitija");
+		al.add("omkar");
+		System.out.println(al);
+	}
+}
